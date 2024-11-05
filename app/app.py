fa5d0e4a5aa5558a611f500bb6cc2b0079b0456d
naman <namanbiswajit.code@gmail.com>
2024-11-05 22:26:42 +0530

    Jinja2 implementation & video player

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("home.html")
 
+@app.route("/watch")
+def video_player():
+    query = request.args.get('play')
+    return render_template("player.html", query=query)
 
 if __name__ == "__main__":
     app.run(debug=True)
