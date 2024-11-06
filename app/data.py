3fb0334cb9df02cc35619c0cbf0c80e4f66ea04b
naman <namanbiswajit.code@gmail.com>
2024-11-06 13:59:03 +0530

    0.2.0v -> rewrote api requests and other minor changes

--- app/data.py
+++ app/data.py
@@ -1,41 +1,58 @@
 import os
-
+import json
 from dotenv import load_dotenv
-from apiclient.discovery import build
+from googleapiclient.discovery import build
+from pprint import pprint
 
-
-load_dotenv()
 
 class YT_Data():
     def __init__(self):
+        load_dotenv()
         self.youtube = build("youtube", "v3", developerKey=os.environ.get("API_KEY"))
-        self.search = self.youtube.search()
+        self.searcher = self.youtube.search()
+        self.videos = self.youtube.videos()
     
-    def reorganise(self, raw_results):
-        res = {}
-        res["data"] = []
+    def format_data(self, data: str, single_request=False):
         
-        for item in raw_results["items"]:
+        processed = []
+        
+        for item in data["items"]:
+            if single_request:
+                kind = item["kind"].split('#')[1]
+            else:
+                kind = item["id"]["kind"].split('#')[1]
             
-            type = item["id"]["kind"].split("#")[1]
-            id = list(item["id"].values())[1]
+            snippet = item["snippet"]
+            id = item["id"]
             
-            res["data"].append({
-                        "id" : id,
-                        "kind" : type,
-                        "title": item["snippet"]["title"],
-                        "description": item["snippet"]["description"],
-                        "thumbnail": item["snippet"]["thumbnails"]["medium"]["url"],
-                        "channelId": item["snippet"]["channelId"],
-                        "channel": item["snippet"]["channelTitle"],
-                        "publishedAt": item["snippet"]["publishedAt"]
-                        })
-            
-        return res
+            processed.append({
+                "kind" : kind,
+                "id" : id,
+                "title" : snippet["title"],
+                "description": snippet["description"],
+                "published_at": snippet["publishedAt"],
+                "channel_name": snippet["channelTitle"],
+                "channel_id": snippet["channelId"]
+                })
 
-    def find(self, query, part="snippet", **params):
-        req = self.search.list(q=query, part=part, **params)
-        results = req.execute()
-        results = self.reorganise(results)
+        return processed
+    
         
+    def search(self, query, part="snippet", **params):
+        req = self.searcher.list(q=query, part=part, **params)
+        results = req.execute()
+        results = self.format_data(results)
         return results
+    
+    def fetch_video(self, query, part="snippet"):
+        req = self.videos.list(id=query, part=part)
+        results = req.execute()
+        results = self.format_data(results, single_request=True)
+        return results
+    
+if __name__ == "__main__":
+    api = YT_Data()
+    result = api.fetch_video(query="ZT1MVkKLZh0")
+    result2 = api.search("Science Videos")
+    pprint(result)
+    pprint(result2)
